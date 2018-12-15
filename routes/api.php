2aa896b1dba375f5c7dbe46a6992a45301cd83d1
lonycn <lonycn@qq.com>
2018-12-15 10:27:13 +0800

    add ArraySerializer

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,58 @@ use Illuminate\Http\Request;
 $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', [
-    'namespace' => 'App\Http\Controllers\Api'
-], function($api) {
-    $api->get('version',function(){
-       return 'v1';
+    'namespace' => 'App\Http\Controllers\Api',
+    'middleware' => 'serializer:array'
+], function ($api) {
+    $api->group([
+        'middleware' => ['api.throttle'],
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        $api->get('version', function () {return response(['version'=>'v1.02']);});
+
+
+        //微信sdk
+        $api->post('wxconfig', 'WeController@wxconfig')->name('api.we.wxconfig');
+        //微信网页授权
+        $api->get('wxauth', 'WeController@wxauth')->name('api.we.wxauth');
+
+        
+        $api->get('time', 'CheckController@index')->name('api.check.time');
+        // 登录
+        $api->post('authorizations', 'AuthorizationsController@store')
+            ->name('api.authorizations.store');
+        // 刷新token
+        $api->put('authorizations/current', 'AuthorizationsController@update')
+            ->name('api.authorizations.update');
+        // 删除token
+        $api->delete('authorizations/current', 'AuthorizationsController@destroy')
+            ->name('api.authorizations.destroy');
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 所有单位
+            $api->get('companies', 'CompaniesController@index')->name('api.companies.index');
+            // 当前单位
+            $api->get('companies/current', 'CompaniesController@current')->name('api.companies.current');
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')->name('api.users.show');
+            // 所有冰箱
+            $api->get('coolers', 'CoolersController@index')->name('api.coolers.index');
+            $api->get('coolers/{cooler}', 'CoolersController@show')->name('api.coolers.show');
+            $api->get('coolers/{cooler}/history', 'CoolersController@history')->name('api.coolers.history');
+            // 所有探头
+            $api->get('collectors', 'CollectorsController@index')->name('api.collectors.index');
+            // 所有联系人
+            $api->get('contacts', 'ConcatsController@index')->name('api.contacts.index');
+            // 实时温湿度
+            $api->get('collectors/realtime', 'CollectorsController@realtime')->name('api.collectors.realtime');
+            //同步数据，获取data_id之后的新数据
+//            $api->post('collectors/sync',function (){
+//                return response(['_SERVER'=>json_encode($_SERVER)]);
+//            });
+            $api->post('collectors/sync', 'CollectorsController@sync')->name('api.collectors.sync');
+            //同步基础数据 -- collector
+            $api->post('tables_syncs', 'TablesSyncsController@index')->name('api.table_syncs.index');
+        });
     });
-    //微信sdk
-    $api->post('wxconfig', 'WeController@wxconfig')->name('api.we.wxconfig');
-    //微信网页授权
-    $api->get('wxauth', 'WeController@wxauth')->name('api.we.wxauth');
 });
