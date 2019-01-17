f17b67dd25d6c7094773b49eb3f68698122b0815
lonycn <lonycn@qq.com>
2019-01-17 17:59:36 +0800

    companies tree add top

--- app/Http/Controllers/Api/Ccms/CompaniesController.php
+++ app/Http/Controllers/Api/Ccms/CompaniesController.php
@@ -63,7 +63,8 @@ class CompaniesController extends Controller
     {
         $this->check($id);
         $company = Company::whereIn('id', $this->company_ids)->select('id', 'pid', 'title', 'short_title')->get();
-        $menus = (new Company())->toTree($company->toArray(), $this->company->id);
+        $companies = $company->toArray();
+        $menus = (new Company())->toTree($company->toArray());
         return $this->response->array($menus);
     }
 
