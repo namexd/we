3d7cca36e2d79e2486c4ab882a2266de6c2816db
lonycn <lonycn@qq.com>
2019-01-27 13:45:54 +0800

    sendlog changeto pgsql

--- app/Models/Ccrp/Coldchain2pgModel.php
+++ app/Models/Ccrp/Coldchain2pgModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Coldchain2pgModel extends Model
 {
-    protected $connection = 'dbhistory';
+    protected $connection = 'pgyingyong';
 
     public function getUpdatedAtColumn()
     {
