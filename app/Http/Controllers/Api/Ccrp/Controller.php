626ef633021418cbfb4610da898e3a883c02ca71
lonycn <lonycn@qq.com>
2019-04-24 11:30:13 +0800

    ccrp_支持url中的company_id参数

--- app/Http/Controllers/Api/Ccrp/Controller.php
+++ app/Http/Controllers/Api/Ccrp/Controller.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api\Ccrp;
+
 use App\Models\Ccrp\Company;
 use App\Models\Ccrp\User;
 use App\Models\UserHasApp;
@@ -8,7 +9,7 @@ use App\Http\Controllers\Api\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    public $app_id=1;
+    public $app_id = 1;
     public $user;
     public $company;
     public $company_ids;
@@ -17,33 +18,34 @@ class Controller extends BaseController
     {
     }
 
-    public function check($company_id=null)
+    public function check($company_id = null)
     {
         $ucenter_user = $this->user();
-        $user_app = UserHasApp::where('user_id',$ucenter_user->id)->where('app_id',$this->app_id)->first();
+        $user_app = UserHasApp::where('user_id', $ucenter_user->id)->where('app_id', $this->app_id)->first();
 
-        if($user_app ==null)
-        {
+        if ($user_app == null) {
             return $this->response->error('系统账号绑定错误', 457);
         }
-        $user = User::where('id',$user_app->app_userid)->first();
+        $user = User::where('id', $user_app->app_userid)->first();
 
         if ($user->status == 0) {
             return $this->response->error('系统账号验证错误', 457);
         } else {
 
-            if($company_id == null)
-            {
-                $company_id = $user->company_id;
-            }else{
+            if ($company_id == null) {
+                if (request()->get('company_id')) {
+                    $company_id = request()->get('company_id');
+                }else{
+                    $company_id = $user->company_id;
+                }
+
+            } else {
                 $user_company = $user->userCompany;
-                if(!$user_company)
-                {
+                if (!$user_company) {
                     return $this->response->error('系统账号验证错误', 457);
                 }
                 $user_company_ids = $user_company->ids();
-                if(!in_array($company_id,$user_company_ids))
-                {
+                if (!in_array($company_id, $user_company_ids)) {
                     $company_id = $user->company_id;
                 }
             }
