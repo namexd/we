ad2909d3d381078204e3c7eeb975eff47c26b20c
lonycn <lonycn@qq.com>
2019-05-15 15:53:18 +0800

    admin运营数据-增加今日，昨日，本月新增用户数

--- app/Http/Controllers/Api/Admin/InfoController.php
+++ app/Http/Controllers/Api/Admin/InfoController.php
@@ -14,13 +14,21 @@ class InfoController extends Controller
         $info['count_users_verified'] = User::where('phone_verified','>',0)->count();
         $info['count_users_today'] = User::where('created_at','>',Carbon::today())->count();
         $info['count_users_verified_today'] = User::where('phone_verified','>',0)->where('created_at','>',Carbon::today())->count();
+        $info['count_users_yesterday'] = User::where('created_at','>',Carbon::yesterday())->count();
+        $info['count_users_verified_yesterday'] = User::where('phone_verified','>',0)->where('created_at','>',Carbon::yesterday())->count();
+        $info['count_users_month'] = User::where('created_at','>',Carbon::now()->firstOfMonth())->count();
+        $info['count_users_verified_month'] = User::where('phone_verified','>',0)->where('created_at','>',Carbon::now()->firstOfMonth())->count();
         $info['new_sers'] = User::orderBy('id','desc')->with('weuser')->limit(5)->get();
 
         $columns = [];
         $columns['count_users'] = '当前注册用户数';
-        $columns['count_users_verified'] = '已验证手机号用户数';
+        $columns['count_users_verified'] = '已认证用户数';
         $columns['count_users_today'] = '今日注册用户数';
-        $columns['count_users_verified_today'] = '今日已验证手机号用户数';
+        $columns['count_users_verified_today'] = '今日已认证用户数';
+        $columns['count_users_yesterday'] = '昨日注册用户数';
+        $columns['count_users_verified_yesterday'] = '昨日已认证用户数';
+        $columns['count_users_month'] = '本月注册用户数';
+        $columns['count_users_verified_month'] = '本月已认证用户数';
         $columns['new_sers'] = '最新注册用户';
 
         $info['meta']['columns'] = $columns;
