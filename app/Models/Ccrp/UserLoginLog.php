2baeb539044e43b10089e36ee440e9cbef296f7f
lonycn <lonycn@qq.com>
2019-05-22 20:59:40 +0800

    ccrp 登录日志增加 真实姓名

--- app/Models/Ccrp/UserLoginLog.php
+++ app/Models/Ccrp/UserLoginLog.php
@@ -4,31 +4,28 @@
 namespace App\Models\Ccrp;
 
 use App\Models\UserHasApp;
+use App\Models\Weapp;
 
 class UserLoginLog extends Coldchain2Model
 {
     protected $table = 'user_login_log';
+
     public static function addCcrpLoginLog($user)
     {
-        $apps = $user->apps;
-        if ($apps) {
-            foreach ($apps as $app) {
-                switch ($app->slug) {
-                    case  'ccrp':
-                        $user_app = UserHasApp::where('user_id', $user->id)->where('app_id', $app->id)->first();
-                        if ($user_app) {
-                            $ccrp_user = User::where('id', $user_app->app_userid)->where('status', 1)->first();
-
-                            self::addLog($ccrp_user->id, $ccrp_user->username ,$ccrp_user->userlevel ,$ccrp_user->company_id,5,time(), request()->ip());
-                            self::addLogPg($ccrp_user->id, $ccrp_user->username ,$ccrp_user->userlevel ,$ccrp_user->company_id,5,time(), request()->ip());
-                        }
-                        break;
-                }
+        $apps = $user->apps->where('slug', 'ccrp');
+        if (isset($apps[0])) {
+            $app = $apps[0];
+            $user_app = UserHasApp::where('user_id', $user->id)->where('app_id', $app->id)->first();
+            if ($user_app) {
+                $ccrp_user = User::where('id', $user_app->app_userid)->where('status', 1)->first();
+                self::addLog($ccrp_user->id, $ccrp_user->username, $ccrp_user->userlevel, $ccrp_user->company_id, 5, time(), request()->ip(), $user->realname);
+                self::addLogPg($ccrp_user->id, $ccrp_user->username, $ccrp_user->userlevel, $ccrp_user->company_id, 5, time(), request()->ip(), $user->realname);
             }
         }
 
     }
-    private static function addLog($uid,$username,$userlevel,$company_id,$type,$login_time,$ip)
+
+    private static function addLog($uid, $username, $userlevel, $company_id, $type, $login_time, $ip, $note)
     {
         $log = new self;
         $log->uid = $uid;
@@ -38,9 +35,11 @@ class UserLoginLog extends Coldchain2Model
         $log->type = $type;
         $log->login_time = $login_time;
         $log->ip = $ip;
+        $log->note = $note;
         $log->save();
     }
-    private static function addLogPg($uid,$username,$userlevel,$company_id,$type,$login_time,$ip)
+
+    private static function addLogPg($uid, $username, $userlevel, $company_id, $type, $login_time, $ip, $note)
     {
         $log = new UserLoginLogPg();
         $log->uid = $uid;
@@ -50,6 +49,7 @@ class UserLoginLog extends Coldchain2Model
         $log->type = $type;
         $log->login_time = $login_time;
         $log->ip = $ip;
+        $log->note = $note;
         $log->save();
     }
 
