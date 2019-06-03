7580f0c83d0f405b646794a4eae131405783fe0c
lonycn <lonycn@qq.com>
2019-06-03 16:47:04 +0800

    ccrp 报警通道显示报警人员名称

--- app/Transformers/Ccrp/WarningerTransformer.php
+++ app/Transformers/Ccrp/WarningerTransformer.php
@@ -24,15 +24,31 @@ class WarningerTransformer extends TransformerAbstract
         ];
         if( in_array($rs['warninger_type'],['短信','电话']))
         {
-            $contacts = Contact::where('company_id',$setting->company_id)-pluck('phone','name');
-            dd($contacts);
-            $rs['warninger_body'] = hidePhone($setting->warninger_body);
-            $rs['warninger_body_level2'] = hidePhone($setting->warninger_body_level2);
-            $rs['warninger_body_level3'] = hidePhone($setting->warninger_body_level3);
+            $contacts = Contact::where('company_id',$setting->company_id)->pluck('name','phone');
+            $rs['warninger_body'] = $this->formatPhone($setting->warninger_body,$contacts);
+            $rs['warninger_body_level2'] = $this->formatPhone($setting->warninger_body_level2,$contacts);
+            $rs['warninger_body_level3'] = $this->formatPhone($setting->warninger_body_level3,$contacts);
         }
         if(request()->get('with'))
         {
             $rs['meta'] = ['header' => $setting->warninger_name];
+        }
+        return $rs;
+    }
+
+    private function formatPhone($phones_str,$contacts)
+    {
+        if($phones_str=="")return "";
+        $rs = "";
+        if(strpos($phones_str,','))
+        {
+            $phones = explode(',',$phones_str);
+            foreach($phones as $phone)
+            {
+                $rs .= $contacts[$phone]? $contacts[$phone]."(".hidePhone($phone).")":hidePhone($phone);
+            }
+        }else{
+            $rs = $contacts[$phones_str]? $contacts[$phones_str]."(".hidePhone($phones_str).")":hidePhone($phones_str);
         }
         return $rs;
     }
