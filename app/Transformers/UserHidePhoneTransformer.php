b31e8fa7bac7b7d0c60e8c12ee4cd283dff02cdf
lonycn <lonycn@qq.com>
2019-06-06 12:44:41 +0800

    显示用户信息手机号隐藏

--- app/Transformers/UserHidePhoneTransformer.php
+++ app/Transformers/UserHidePhoneTransformer.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use function App\Utils\hidePhone;
 use League\Fractal\TransformerAbstract;
 
-class UserTransformer extends TransformerAbstract
+class UserHidePhoneTransformer extends TransformerAbstract
 {
     public function transform(User $user)
     {
@@ -14,7 +14,7 @@ class UserTransformer extends TransformerAbstract
             'id' => $user->id,
             'name' => $user->name,
             'realname' => $user->realname,
-            'phone' => $user->phone,
+            'phone' => hidePhone($user->phone),
             'phone_verified' => $user->phone_verified ,
             'headimgurl'=>$user->weuser->headimgurl,
             'created_at' => $user->created_at->toDateTimeString(),
